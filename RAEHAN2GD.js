723afd3b19a194b5b0ff1fff9e922a4ed7a88194
HANZ-2GD <hanz.26r@gmail.com>
2026-03-30 02:32:12 +0800

    Update RAEHAN2GD.js

--- RAEHAN2GD.js
+++ RAEHAN2GD.js
@@ -203,7 +203,7 @@ case 'setppgchanz': {
 			break
 	    ////////////////////////𝙃𝘼𝙉𝙕///2𝙂𝘿////////////////////////////
 
-		case 'insta' : case 'instagram' :  { m.reply { image: {url: 'https://telegra.ph/file/7b8b904ecabdbe0744635.jpg'}('https://www.instagram.com/hanz_932?igsh=Ymp6dTNjYzhtODFq')}
+		case 'insta' : case 'instagram' :  { m.reply { image: {url: 'https://telegra.ph/file/7b8b904ecabdbe0744635.jpg'}('https://www.instagram.com/hanz_932?igsh=Ymp6dTNjYzhtODFq')}}
 		
 		break
 			// Menu
