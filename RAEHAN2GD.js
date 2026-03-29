bd5ca08b303d6ced15854ed901a75a90442018e1
HANZ-2GD <hanz.26r@gmail.com>
2026-03-30 03:20:52 +0800

    Update RAEHAN2GD.js

--- RAEHAN2GD.js
+++ RAEHAN2GD.js
@@ -203,11 +203,20 @@ case 'setppgchanz': {
 			break
 	    ////////////////////////𝙃𝘼𝙉𝙕///2𝙂𝘿////////////////////////////
 
-		case 'insta' : case 'instagram' :  {
-			const hanzzz =`https://www.instagram.com/hanz_932?igsh=Ymp6dTNjYzhtODFq`
+
+
+		
+		/*case 'insta' : case 'instagram' :  {
+			const hanzzz =`
+        
+		https://www.instagram.com/hanz_932?igsh=Ymp6dTNjYzhtODFq`
 		RAEHAN2GD.sendMessage(m.chat, { image: {url: 'https://telegra.ph/file/7b8b904ecabdbe0744635.jpg'}, 
 caption: hanzzz })}
-		break
+		break*/
+
+
+
+		
 			// Menu
 			
 			case 'hanz' :  { //m.reply (
