a1781a2ddcc0d656b429cfcf54b69b848fe2f669
HANZ-2GD <hanz.26r@gmail.com>
2026-04-01 22:24:35 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,14 +17,7 @@ const { app, server, PORT } = require('./src/server');
 const { GroupParticipantsUpdate, MessagesUpsert, Solving } = require('./src/message');
 
 
-const moment = require('moment-timezone');
-const { getRandom, getBuffer, fetchJson, runtime, clockString, sleep, isUrl, formatDate, formatp, generateProfilePicture, errorCache, normalize, updateSettings, parseMention, fixBytes, similarity, pickRandom, tarBackup, unsafeAgent } = require('./DataBoss/function');
-	const hari = moment.tz('Asia/Jakarta').locale('id').format('dddd');
-		const tanggal = moment.tz('Asia/Jakarta').locale('id').format('DD/MM/YYYY');		
-		const jamjak = moment.tz('Asia/Jakarta').locale('id').format('HH:mm:ss');
-		const jammak = moment.tz('Asia/Makassar').locale('id').format('HH:mm:ss');
-		const jamjay = moment.tz('Asia/Jayapura').locale('id').format('HH:mm:ss');
-
+const { unsafeAgent } = require('./DataBoss/function');a
 
 
 const print = (label, value) => console.log(`${chalk.green.bold('┃')} ${chalk.cyan.bold(label.padEnd(16))}${chalk.yellow.bold(':')} ${value}`);
@@ -264,7 +257,7 @@ async function startRAEHAN2GDBot() {
 		if (global.db?.set[botNumber]?.anticall) {
 			for (let id of call) {
 				if (id.status === 'offer') {
-					let msg = await RAEHAN2GD.sendMessage(id.from, { text: `╭━━━━━━━━━━━━━╾•\n┃𝙷𝙰𝙻𝙻𝙾 𝙼𝙰𝚂 / 𝙼𝙱𝙰𝙺\n┃ @${id.from.split('@')[0]}\n┣━━━━━━━━━━━━━━•\n┃ 𝘗𝘈𝘕𝘎𝘎𝘐𝘓𝘈𝘕  ${id.isVideo ? 'Video' : 'Suara'}\n┣━━━━━━━━━━━━━━•\n┃𝙼𝚊𝚊𝚏 𝙼𝚊𝚜 / 𝙼𝚋𝚊𝚔\n┃𝙿𝚎𝚖𝚒𝚕𝚒𝚔 𝚜𝚎𝚍𝚊𝚗𝚐\n┃𝚃𝚒𝚍𝚊𝚔 𝚖𝚎𝚖𝚋𝚊𝚠𝚊\n┃𝙷𝙿 / 𝙷𝚊𝚗𝚍𝚙𝚑𝚘𝚗𝚎\n┃𝚃𝚘𝚕𝚘𝚗𝚐\n┃𝚃𝚒𝚗𝚐𝚐𝚊𝚕𝚔𝚊𝚗 𝚙𝚎𝚜𝚊𝚗\n┣━━━━━━━━━━━━━━•\n┃ɪɴɪ  ᴀᴅᴀʟᴀʜ  ᴋᴇᴄᴇʀᴅᴀsᴀɴ  ʙᴜᴀᴛᴀɴ\n┃ᴅɪʙᴜᴀᴛ  ᴏʟᴇʜ  ʀᴀᴇʜᴀɴ\n╰━━━━━━━━━━━━━━╯\n\n\n\n╭━━━━━━━━━━━━━╾•\n┃ᴡᴀᴋᴛᴜ ᴘᴀɴɢɢɪʟᴀɴ ᴍᴀsᴜᴋ\n┣━━━━━━━━━━━━━━•\n┃│⃟•╾ ◎ ʜᴀʀɪ : ${hari}\n┃│⃟•╾ ◎ ᴛɢʟ  : ${tanggal}\n┣━━━━━━━━━━━━━━•\n┃│⃟•╾ ◎ ᴊᴀᴍ   ${jamjak} WIB\n┃│⃟•╾ ◎ ᴊᴀᴍ   ${jammak} WITA\n┃│⃟•╾ ◎ ᴊᴀᴍ   ${jamjay} WIT\n╰━━━━━━━━━━━━━━╯`, mentions: [id.from]});
+					let msg = await RAEHAN2GD.sendMessage(id.from, { text: `╭━━━━━━━━━━━━━╾•\n┃𝙷𝙰𝙻𝙻𝙾 𝙼𝙰𝚂 / 𝙼𝙱𝙰𝙺\n┃ @${id.from.split('@')[0]}\n┣━━━━━━━━━━━━━━•\n┃ 𝘗𝘈𝘕𝘎𝘎𝘐𝘓𝘈𝘕  ${id.isVideo ? 'Video' : 'Suara'}\n┣━━━━━━━━━━━━━━•\n┃𝙼𝚊𝚊𝚏 𝙼𝚊𝚜 / 𝙼𝚋𝚊𝚔\n┃𝙿𝚎𝚖𝚒𝚕𝚒𝚔 𝚜𝚎𝚍𝚊𝚗𝚐\n┃𝚃𝚒𝚍𝚊𝚔 𝚖𝚎𝚖𝚋𝚊𝚠𝚊\n┃𝙷𝙿 / 𝙷𝚊𝚗𝚍𝚙𝚑𝚘𝚗𝚎\n┃𝚃𝚘𝚕𝚘𝚗𝚐\n┃𝚃𝚒𝚗𝚐𝚐𝚊𝚕𝚔𝚊𝚗 𝚙𝚎𝚜𝚊𝚗\n┣━━━━━━━━━━━━━━•\n┃ɪɴɪ  ᴀᴅᴀʟᴀʜ  ᴋᴇᴄᴇʀᴅᴀsᴀɴ  ʙᴜᴀᴛᴀɴ\n┃ᴅɪʙᴜᴀᴛ  ᴏʟᴇʜ  ʀᴀᴇʜᴀɴ\n╰━━━━━━━━━━━━━━╯`, mentions: [id.from]});
 					
 					await RAEHAN2GD.rejectCall(id.id, id.from)
 				}
