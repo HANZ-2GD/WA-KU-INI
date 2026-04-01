220cc42e9733d8bbad06b99a796460c13876f6b4
HANZ-2GD <hanz.26r@gmail.com>
2026-04-01 21:57:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const { dataBase } = require('./src/database');
 const { app, server, PORT } = require('./src/server');
 const { GroupParticipantsUpdate, MessagesUpsert, Solving } = require('./src/message');
 
-setInterval(() => {
 
 const moment = require('moment-timezone');
 const { getRandom, getBuffer, fetchJson, runtime, clockString, sleep, isUrl, formatDate, formatp, generateProfilePicture, errorCache, normalize, updateSettings, parseMention, fixBytes, similarity, pickRandom, tarBackup, unsafeAgent } = require('./DataBoss/function');
@@ -26,7 +25,6 @@ const { getRandom, getBuffer, fetchJson, runtime, clockString, sleep, isUrl, for
 		const jammak = moment.tz('Asia/Makassar').locale('id').format('HH:mm:ss');
 		const jamjay = moment.tz('Asia/Jayapura').locale('id').format('HH:mm:ss');
 
-}, 1000); // Update setiap 1000ms (1 detik)
 
 
 const print = (label, value) => console.log(`${chalk.green.bold('┃')} ${chalk.cyan.bold(label.padEnd(16))}${chalk.yellow.bold(':')} ${value}`);
